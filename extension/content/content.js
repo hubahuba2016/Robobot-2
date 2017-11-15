dd19b662c0e92e0048cac9fa0cb54361246caf10
mb48962 <mb48962@vt.edu>
2017-11-15 18:33:35 -0500

    Generate bot count and injects on profile page and timeline

--- extension/content/content.js
+++ extension/content/content.js
@@ -1,131 +1,98 @@
-var tweets = document.querySelectorAll('div.tweet');
-var users = {};
-var tweetsy = {};
+// send to server ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-for (var i = 0; i < tweets.length; i++) {
-	var user_id = tweets[i].getAttribute('data-user-id');
-	var screen_name = tweets[i].getAttribute('data-screen-name');
-	var tweetsy_id = tweets[i].getAttribute('data-tweet-id');
-	tweetsy[i] = tweetsy_id;
-	users[user_id] = screen_name;
+// check if on home or profile page
+var url = window.location.pathname;
+var profile = url.split("/").slice(-1)[0];
+if (profile === undefined || profile === '') {
+	var ur = document.querySelector('.DashboardProfileCard-content .DashboardProfileCard-userFields .DashboardProfileCard-screenname .DashboardProfileCard-screennameLink .username .u-linkComplex-target').innerHTML;
+	profile = ur;
 }
 
-for (key in users) {
-	if (users[key] != null) {
-		console.log(users[key]);
-		poster(users[key]);
+tweet('926553250064687109', 'Retweets');
+tweet('926553250064687109', 'Likes');
+follow(profile, 'Followers');
+follow(profile, 'Following');
+
+var result;
+
+function tweet(name, redir) {
+	console.log(`running tweet(${name}, ${redir})`);
+	chrome.runtime.sendMessage({
+        method: 'POST',
+        action: 'xhttp',
+        url: 'http://localhost:5000/check_post/' + redir,
+        data: JSON.stringify(name)
+    }, function(responseText) {
+			result = responseText;
+			console.log(`TWEET result: ${result}`);
+			if (result !== undefined) inject(result, '926553250064687109', redir);
+    });
+}
+
+function follow(name, redir) {
+	console.log(`running follow(${name}, ${redir})`);
+	chrome.runtime.sendMessage({
+        method: 'POST',
+        action: 'xhttp',
+        url: 'http://localhost:5000/follow/' + redir,
+        data: JSON.stringify(name)
+    }, function(responseText) {
+			result = responseText;
+			console.log(`FOLLOW result: ${result}`);
+			if (result !== undefined) inject(result, '' ,redir);
+    });
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Front-End Inject
+function inject(val, tweetID, act) {
+	if (act === 'Retweets') {
+		var tw = document.querySelector('div.tweet[data-tweet-id="'+tweetID+'"]');
+		// var ele = tw.querySelector('.content').querySelector('.stream-item-footer').querySelector('.ProfileTweet-actionList').querySelector('.ProfileTweet-action--retweet').querySelector('.ProfileTweet-actionButtonUndo').querySelector('.ProfileTweet-actionCount');
+		var ele = tw.querySelector('.content .stream-item-footer .ProfileTweet-actionList .ProfileTweet-action--retweet .ProfileTweet-actionButtonUndo .ProfileTweet-actionCount');
+		var old_val = ele.querySelector('.ProfileTweet-actionCountForPresentation').innerHTML
+		var bots = ele.innerHTML = `<span class="ProfileTweet-actionCountForPresentation" aria-hidden="true"> ${val}/${old_val}</span>`;
 	}
-}
-
-// for (key in tweetsy) {
-// 	if (tweetsy[key] != null) {
-// 		console.log(tweetsy[key]);
-// 		retweeter(tweetsy[key]);
-// 	}
-// }
-
-// $(tweets[0]).append('<button id="myBtn">Open Modal</button>');
-
-
-// $('body').append('<div id="myModal" class="mod"> <div class="mod-content"> <span class="close">&times;</span> <p>Some text in the Modal..</p> </div> </div>');
-
-// // Get the modal
-// var modal = document.getElementById('myModal');
-// console.log(modal);
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-// console.log(btn);
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on the button, open the modal 
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
-
-function poster(name) {
-	chrome.runtime.sendMessage({
-        method: 'POST',
-        action: 'xhttp',
-        url: 'http://localhost:5000/is_user_bot',
-        data: JSON.stringify(name)
-    }, function(responseText) {
-    		var tweets = document.querySelectorAll('div.tweet');
-
-			for (var i = 0; i < tweets.length; i++) {
-				var screen_name = tweets[i].getAttribute('data-screen-name');
-
-				if (screen_name === name) {
-					var node = document.createElement('small');
-					var textnode = document.createTextNode(responseText);
-					node.appendChild(textnode);
-					node.style.display = 'inline';
-					tweets[i].querySelector('.stream-item-header').appendChild(node);
-
-
-					if (responseText === 'bot') {
-						var group = tweets[i].querySelector('.account-group.js-account-group.js-action-profile.js-user-profile-link.js-nav');
-
-						var avatar = tweets[i].querySelector('.avatar.js-action-profile-avatar');
-
-						var badge = document.createElement('img');
-						badge.src = chrome.extension.getURL("icons/badge.png");
-						badge.classList.add('avatar');
-						badge.classList.add('badge');
-
-						var wrapper = document.createElement('div');
-						group.insertBefore(wrapper, group.firstChild);
-						wrapper.appendChild(avatar);
-						wrapper.appendChild(badge);
-
-						// var div = document.createElement('div');
-						// // var textnode = document.createTextNode('bot');
-						// // div.appendChild(textnode);
-						// div.classList.add('bot');
-						// tweets[i].appendChild(div);
-						// var img = document.createElement('img');
-						// img.src = chrome.extension.getURL("icons/icon.png");
-						// img.classList.add('center');
-						// img.classList.add('spin');
-						// div.appendChild(img);
-					}
-				}
-			}
-    });
-}
-
-function retweeter(name) {
-	chrome.runtime.sendMessage({
-        method: 'POST',
-        action: 'xhttp',
-        url: 'http://localhost:5000/check_post',
-        data: JSON.stringify(name)
-    }, function(responseText) {
-    		var tweets = document.querySelectorAll('div.tweet');
-
-			for (var i = 0; i < tweets.length; i++) {
-				var post_id = tweets[i].getAttribute('data-tweet-id');
-				if (post_id === name) {
-					var node = document.createElement("P");
-					var textnode = document.createTextNode(responseText);
-					node.appendChild(textnode);
-					node.style.display = 'inline';
-					tweets[i].querySelector('.stream-item-footer').appendChild(node);
-				}
-			}
-    });
+	else if (act === 'Likes') {
+		var tw = document.querySelector('div.tweet[data-tweet-id="'+tweetID+'"]');
+		// var ele = tw.querySelector('.content').querySelector('.stream-item-footer').querySelector('.ProfileTweet-actionList').querySelector('.ProfileTweet-action--favorite').querySelector('.ProfileTweet-actionButton').querySelector('.ProfileTweet-actionCount');
+		var ele = tw.querySelector('.content .stream-item-footer .ProfileTweet-actionList .ProfileTweet-action--favorite .ProfileTweet-actionButton .ProfileTweet-actionCount');
+		var old_val = ele.querySelector('.ProfileTweet-actionCountForPresentation').innerHTML
+		var bots = ele.innerHTML = `<span class="ProfileTweet-actionCountForPresentation" aria-hidden="true"> ${val}/${old_val}</span>`;
+	}
+	else if (act === 'Followers') {
+		var ur = window.location.pathname;
+		var prof = ur.split("/").slice(-1)[0];
+		// on profile
+		if (prof) {
+			var ele = document.querySelector('.ProfileNav .ProfileNav-list .ProfileNav-item--followers .ProfileNav-stat');
+			var old_val = ele.querySelector('.ProfileNav-value').innerHTML;
+			var bots = ele.innerHTML = `<span class="ProfileNav-label" aria-hidden="true">Followers</span> <span class="ProfileNav-value" data-is-compact="false">${val}/${old_val}</span>`;
+		}
+		// on home
+		else {
+			var ele = document.querySelector('.DashboardProfileCard-content .ProfileCardStats .ProfileCardStats-statList');
+			var li = ele.querySelectorAll('.ProfileCardStats-stat')[2];
+			var lio = li.querySelector('.ProfileCardStats-statLink .ProfileCardStats-statValue');
+			var old_val = lio.innerHTML;
+			var bots = lio.innerHTML = `<span class="ProfileCardStats-statValue" data-is-compact="false">${val}/${old_val}</span>`;
+		}
+	}
+	else if (act === 'Following') {
+		var ur = window.location.pathname;
+		var prof = ur.split("/").slice(-1)[0];
+		// on profile
+		if (prof) {
+			var ele = document.querySelector('.ProfileNav .ProfileNav-list .ProfileNav-item--following .ProfileNav-stat');
+			var old_val = ele.querySelector('.ProfileNav-value').innerHTML;
+			var bots = ele.innerHTML = `<span class="ProfileNav-label" aria-hidden="true">Following</span> <span class="ProfileNav-value" data-is-compact="false">${val}/${old_val}</span>`;
+		}
+		// on home
+		else {
+			var ele = document.querySelector('.DashboardProfileCard-content .ProfileCardStats .ProfileCardStats-statList');
+			var li = ele.querySelectorAll('.ProfileCardStats-stat')[1];
+			var lio = li.querySelector('.ProfileCardStats-statLink .ProfileCardStats-statValue');
+			var old_val = lio.innerHTML;
+			var bots = lio.innerHTML = `<span class="ProfileCardStats-statValue" data-is-compact="false">${val}/${old_val}</span>`;
+		}
+	}
 }
