c69c53e6aa6bf8f42fe31afcebd7eb8e847797db
DESKTOP-4HLO6HI\Tanner <ltanner3@vt.edu>
2017-11-15 23:10:07 -0500

    Push M + T

--- extension/popup/inject.js
+++ extension/popup/inject.js
@@ -1,5 +1,3 @@
-var configure = require("../content/content.js");
-
 function checkProfile() {
 	var url = window.location.pathname;
     var profile = url.split("/").slice(-1)[0];
@@ -20,14 +18,17 @@ function mouseOut() {
 
 function actionFollower(event) {
    	//alert('1');
-   	configure.profile(checkProfile(), 'Followers');
+   	// jQuery
+	//$.getScript('./../content/content.js',function(){
+	follow(checkProfile(), 'Followers');
+		//});
 	event.preventDefault();
 	event.stopPropagation();
 }
 
 function actionFollowing(event) {
    	//alert('1');
-   	configure.profile(checkProfile(), 'Following')
+   	follow(checkProfile(), 'Following')
 	event.preventDefault();
 	event.stopPropagation();
 }
@@ -37,7 +38,7 @@ function actionRetweet(event) {
    	var split = raw.split("aria-");
    	var id = split[1];
    	//alert(id);
-   	configure.tweet(id, 'Retweets');
+   	tweet(id, 'Retweets');
 	event.stopPropagation();
 }
 
@@ -46,7 +47,7 @@ function actionFavorite(event) {
    	var split = raw.split("aria-");
    	var id = split[1];
    	//alert(id);
-   	configure.tweet(id, 'Likes');
+   	tweet(id, 'Likes');
 	event.stopPropagation();
 }
 
@@ -123,3 +124,87 @@ function highlight() {
 }
 
 highlight();
+
+// Check Retweets or Likes on a tweet
+function tweet(name, redir) {
+	var result;
+	console.log(`running tweet(${name}, ${redir})`);
+	chrome.runtime.sendMessage({
+        method: 'POST',
+        action: 'xhttp',
+        url: 'http://localhost:5000/check_post/' + redir,
+        data: JSON.stringify(name)
+    }, function(responseText) {
+			result = responseText;
+			console.log(`TWEET result: ${result}`);
+			if (result !== undefined) inject(result, '926553250064687109', redir);
+    });
+}
+
+// Check Followers or Following on a profile page or timeline
+function follow(name, redir) {
+	var result;
+	console.log(`running follow(${name}, ${redir})`);
+	chrome.runtime.sendMessage({
+        method: 'POST',
+        action: 'xhttp',
+        url: 'http://localhost:5000/follow/' + redir,
+        data: JSON.stringify(name)
+    }, function(responseText) {
+			result = responseText;
+			console.log(`FOLLOW result: ${result}`);
+			if (result !== undefined) inject(result, '' ,redir);
+    });
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Front-End Inject
+function inject(val, tweetID, act) {
+	if (act === 'Retweets') {
+		var tw = document.querySelector('div.tweet[data-tweet-id="'+tweetID+'"]');
+		var ele = tw.querySelector('.content .stream-item-footer .ProfileTweet-actionList .ProfileTweet-action--retweet .ProfileTweet-actionButtonUndo .ProfileTweet-actionCount');
+		var old_val = ele.querySelector('.ProfileTweet-actionCountForPresentation').innerHTML
+		var bots = ele.innerHTML = `<span class="ProfileTweet-actionCountForPresentation" aria-hidden="true"> ${val}/${old_val}</span>`;
+	}
+	else if (act === 'Likes') {
+		var tw = document.querySelector('div.tweet[data-tweet-id="'+tweetID+'"]');
+		var ele = tw.querySelector('.content .stream-item-footer .ProfileTweet-actionList .ProfileTweet-action--favorite .ProfileTweet-actionButton .ProfileTweet-actionCount');
+		var old_val = ele.querySelector('.ProfileTweet-actionCountForPresentation').innerHTML
+		var bots = ele.innerHTML = `<span class="ProfileTweet-actionCountForPresentation" aria-hidden="true"> ${val}/${old_val}</span>`;
+	}
+	else if (act === 'Followers') {
+		var ur = window.location.pathname;
+		var prof = ur.split("/").slice(-1)[0];
+		// on profile
+		if (prof) {
+			var ele = document.querySelector('.ProfileNav .ProfileNav-list .ProfileNav-item--followers .ProfileNav-stat');
+			var old_val = ele.querySelector('.ProfileNav-value').innerHTML;
+			var bots = ele.innerHTML = `<span class="ProfileNav-label" aria-hidden="true">Followers</span> <span class="ProfileNav-value" data-is-compact="false">${val}/${old_val}</span>`;
+		}
+		// on home
+		else {
+			var ele = document.querySelector('.DashboardProfileCard-content .ProfileCardStats .ProfileCardStats-statList');
+			var li = ele.querySelectorAll('.ProfileCardStats-stat')[2];
+			var lio = li.querySelector('.ProfileCardStats-statLink .ProfileCardStats-statValue');
+			var old_val = lio.innerHTML;
+			var bots = lio.innerHTML = `<span class="ProfileCardStats-statValue" data-is-compact="false">${val}/${old_val}</span>`;
+		}
+	}
+	else if (act === 'Following') {
+		var ur = window.location.pathname;
+		var prof = ur.split("/").slice(-1)[0];
+		// on profile
+		if (prof) {
+			var ele = document.querySelector('.ProfileNav .ProfileNav-list .ProfileNav-item--following .ProfileNav-stat');
+			var old_val = ele.querySelector('.ProfileNav-value').innerHTML;
+			var bots = ele.innerHTML = `<span class="ProfileNav-label" aria-hidden="true">Following</span> <span class="ProfileNav-value" data-is-compact="false">${val}/${old_val}</span>`;
+		}
+		// on home
+		else {
+			var ele = document.querySelector('.DashboardProfileCard-content .ProfileCardStats .ProfileCardStats-statList');
+			var li = ele.querySelectorAll('.ProfileCardStats-stat')[1];
+			var lio = li.querySelector('.ProfileCardStats-statLink .ProfileCardStats-statValue');
+			var old_val = lio.innerHTML;
+			var bots = lio.innerHTML = `<span class="ProfileCardStats-statValue" data-is-compact="false">${val}/${old_val}</span>`;
+		}
+	}
+}
---
+++ server/hello.py
@@ -0,0 +1,127 @@
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+import botometer
+import tweepy
+import urllib
+import urllib.request as ur
+import re
+import json
+
+# Secret keys
+mashape_key = "uKUDX0gpUqmshZ4XfpfE2Kzh9kgSp1nqeCKjsnynmzOcFDF6lw"
+twitter_app_auth = {
+    'consumer_key': '8btgkkENoxJGVPFhuhzWg3QDz',
+    'consumer_secret': 'ay88Z9Wqf7QFL6vDm1CLa7LpAt55EipE3ndoFlzslHuN25pfKW',
+    'access_token': '1517890854-M3KSvLUrlT4pYKc5ftehmTuCS9ukRexYnIbg9rT',
+    'access_token_secret': 'cYMY5Pehr2mKXdpmPn3K7J3xUI4Ifnfsssctdch8nsQv2',
+}
+# Authentication
+auth = tweepy.OAuthHandler(twitter_app_auth['consumer_key'], twitter_app_auth['consumer_secret'])
+auth.set_access_token(twitter_app_auth['access_token'], twitter_app_auth['access_token_secret'])
+# APIs
+bom = botometer.Botometer(mashape_key=mashape_key, **twitter_app_auth)
+api = tweepy.API(auth)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# TWEET()
+def get_user_ids_of_post_likes(post_id):
+    try:
+        json_data = ur.urlopen('https://twitter.com/i/activity/favorited_popup?id=' + str(post_id)).read()
+        found_ids = re.findall(r'data-user-id=\\\\\\"+\d+', json.dumps(json_data.decode('utf-8')))
+        unique_ids = list(set([re.findall(r'\d+', match)[0] for match in found_ids]))
+        return unique_ids
+    except urllib.error.HTTPError:
+        return False
+def check_post_helper(id, act):
+    postObj = api.get_status(id)
+    users = []
+    found = ''
+    if act == 'Likes':
+        found = get_user_ids_of_post_likes(id)
+        # Limit because slow
+        index = 0;
+        for ids in found:
+            if (index < 5):
+                users.append('@' + api.get_user(ids).screen_name)
+            index = index + 1;
+    if act == 'Retweets':
+        found = api.retweets(postObj.id)
+        # Limit because slow
+        index = 0;
+        for name in found:
+            if (index < 5):
+                users.append('@' + name.user.screen_name)
+            index = index + 1;
+    results = list(bom.check_accounts_in(users))
+    return results
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# FOLLOW()
+def get_friends(user_id):
+    users = []
+    page_count = 0
+    for user in tweepy.Cursor(api.friends, id=user_id, count=200).pages():
+        page_count += 1
+        users.extend(user)
+    return users
+def follow_helper(userIn, act):
+    user = api.get_user(screen_name = userIn)
+    accounts = []
+    if act == 'Following':
+        index = 0;
+        for following in get_friends(user.id_str):
+            # Limit accounts to check because slow
+            if (index < 5) :
+                accounts.append('@' + following.screen_name)
+            index = index + 1;
+        results = list(bom.check_accounts_in(accounts))
+    elif act == 'Followers':
+        index = 0;
+        for followers in user.followers():
+            if (index < 5):
+                accounts.append('@' + followers.screen_name)
+            index = index + 1;
+        results = list(bom.check_accounts_in(accounts))
+    count = 0
+    # TODO FIX THIS
+    for score in results:
+        if ('scores' in score[1]) :
+            print('FOLLOW()', score[0], score[1]['scores']['english'])
+            if score[1]['scores']['english'] > 0.4:
+                count = count + 1
+    return str(count)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Flask app
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+# TWEET()
+@app.route("/check_post/<string>", methods=['GET', 'POST'])
+@cross_origin(support_credentials=True)
+def check_post(string):
+    if string == 'Likes':
+        print('getting LIKES')
+        bots = check_post_helper(request.get_json(), 'Likes')
+    elif string == 'Retweets':
+        print('getting RETWEETS')
+        bots = check_post_helper(request.get_json(), 'Retweets')
+    count = 0
+    for bot in bots:
+        if ('scores' in bot[1]):
+            print('TWEET()', bot[0], bot[1]['scores']['english'])
+            if bot[1]['scores']['english'] > 0.4:
+                count = count + 1
+    return str(count)
+# FOLLOW()
+@app.route("/follow/<string>", methods=['GET', 'POST'])
+@cross_origin(support_credentials=True)
+def follow(string):
+    if string == 'Followers':
+        print('getting FOLLOWERS')
+        return follow_helper(request.get_json(), 'Followers')
+    elif string == 'Following':
+        print('getting FOLLOWING')
+        return follow_helper(request.get_json(), 'Following')
+
+if __name__ == "__main__":
+    app.run()
