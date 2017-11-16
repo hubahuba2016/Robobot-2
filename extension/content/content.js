2e7609eae4e7f95ef0bce9950770acea9f3ad093
mb48962 <mb48962@vt.edu>
2017-11-16 04:23:21 -0500

    I fd up

--- extension/content/content.js
+++ extension/content/content.js
@@ -74,71 +74,11 @@ function checkTimeline() {
 			}
 		}
 
-		//processUsers(users);
+		// processUsers(users);
 	}
 }
 
 setInterval(checkTimeline, 1000);
-
-var tweets = document.querySelectorAll('div.tweet');
-$(tweets[0]).append('<button id="myBtn">Open Modal</button>');
-
-
-var modal = document.createElement('div');
-modal.id = 'myModal';
-modal.classList.add('mod');
-document.body.appendChild(modal);
-
-var close = document.createElement('div');
-close.classList.add('PermalinkProfile-dismiss', 'modal-close-fixed');
-modal.appendChild(close);
-
-var closeIcon = document.createElement('span');
-closeIcon.id = 'clo';
-closeIcon.classList.add('Icon', 'Icon--close');
-close.appendChild(closeIcon)
-
-var con = document.createElement('div');
-con.classList.add('mod-content');
-modal.appendChild(con);
-
-var p = document.createElement('p');
-var textnode = document.createTextNode('hiibi');
-p.appendChild(textnode);
-con.appendChild(p);
-
-p = document.createElement('p');
-textnode = document.createTextNode('hiibi');
-p.appendChild(textnode);
-con.appendChild(p);
-
-// Get the modal
-var modal = document.getElementById('myModal');
-console.log(modal);
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-console.log(btn);
-
-// Get the <span> element that closes the modal
-var span = document.getElementById("clo");
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
 
 function poster(name) {
 	chrome.runtime.sendMessage({
@@ -157,9 +97,16 @@ function poster(name) {
 					tweets[i].setAttribute('bot-score', responseText);
 
 					if (responseText === 'bot') {
+						var verified = tweets[i].querySelector('span.Icon.Icon--verified');
+
 						var badge = tweets[i].querySelector('.stream-item-header .badge');
-						badge.src = chrome.extension.getURL("icons/icon48.png");
-						badge.classList.remove('spin');
+						if (verified === null) {
+							badge.src = chrome.extension.getURL("icons/icon48.png");
+						}
+						else {
+							badge.src = chrome.extension.getURL("icons/checked.png");
+						}
+							badge.classList.remove('spin');
 
 						badge.onclick = function(event) {
 							if (event.target.classList.contains("badge")) {
--- extension/popup/inject.js
+++ extension/popup/inject.js
@@ -1,3 +1,63 @@
+var tweets = document.querySelectorAll('div.tweet');
+$(tweets[0]).append('<button id="myBtn">Open Modal</button>');
+
+var modal = document.createElement('div');
+modal.id = 'myModal';
+modal.classList.add('mod');
+document.body.appendChild(modal);
+
+var close = document.createElement('div');
+close.classList.add('PermalinkProfile-dismiss', 'modal-close-fixed');
+modal.appendChild(close);
+
+var closeIcon = document.createElement('span');
+closeIcon.id = 'clo';
+closeIcon.classList.add('Icon', 'Icon--close');
+close.appendChild(closeIcon)
+
+var con = document.createElement('div');
+con.classList.add('mod-content');
+modal.appendChild(con);
+
+var p = document.createElement('p');
+var textnode = document.createTextNode('hiibi');
+p.appendChild(textnode);
+con.appendChild(p);
+
+p = document.createElement('p');
+textnode = document.createTextNode('hiibi');
+p.appendChild(textnode);
+con.appendChild(p);
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Get the modal
+var modal = document.getElementById('myModal');
+console.log(modal);
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+console.log(btn);
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("clo");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 function checkProfile() {
 	var url = window.location.pathname;
     var profile = url.split("/").slice(-1)[0];
@@ -18,37 +78,50 @@ function mouseOut() {
 
 function actionFollower(event) {
    	//alert('1');
-   	// jQuery
-	//$.getScript('./../content/content.js',function(){
-	follow(checkProfile(), 'Followers');
-		//});
-	event.preventDefault();
+    event.preventDefault();
 	event.stopPropagation();
+	this.style = 'none';
+	this.removeEventListener("mouseover", mouseOver);
+	this.removeEventListener("mouseout", mouseOut);
+	this.removeEventListener("click", actionFollower);
+	follow(checkProfile(), 'Followers');
 }
 
 function actionFollowing(event) {
    	//alert('1');
-   	follow(checkProfile(), 'Following')
-	event.preventDefault();
+   	event.preventDefault();
 	event.stopPropagation();
+	this.style = 'none';
+	this.removeEventListener("mouseover", mouseOver);
+	this.removeEventListener("mouseout", mouseOut);
+	this.removeEventListener("click", actionFollowing);
+   	follow(checkProfile(), 'Following');
 }
 
 function actionRetweet(event) {
+    event.stopPropagation();
    	var raw = $(this).attr('aria-describedby')
    	var split = raw.split("aria-");
    	var id = split[1];
    	//alert(id);
+    this.style = 'none';
+	this.removeEventListener("mouseover", mouseOver);
+	this.removeEventListener("mouseout", mouseOut);
+	this.removeEventListener("click", actionRetweet);
    	tweet(id, 'Retweets');
-	event.stopPropagation();
 }
 
 function actionFavorite(event) {
+	event.stopPropagation();
     var raw = $(this).attr('aria-describedby')
    	var split = raw.split("aria-");
    	var id = split[1];
    	//alert(id);
+   	this.style = 'none';
+	this.removeEventListener("mouseover", mouseOver);
+	this.removeEventListener("mouseout", mouseOut);
+	this.removeEventListener("click", actionFavorite);
    	tweet(id, 'Likes');
-	event.stopPropagation();
 }
 
 function highlight() {
@@ -63,40 +136,45 @@ function highlight() {
 		var selected = document.querySelectorAll(highlight[i]);
 
 		for (var element of selected) {
-			//console.log(element.style.zIndex);
-			$(element).wrap( "<span></span>" );
-			element.style.outline = '2.5px solid blue';
-			element.style.backgroundColor = 'lightBlue';
-		    element.addEventListener("mouseover", mouseOver);
-			element.addEventListener("mouseout", mouseOut);
-			if (i == 0)
+			if (element.style.outline == '')
 			{
-			    element.addEventListener("click", actionRetweet);
-			}
-			if (i == 1)
-			{
-				element.addEventListener("click", actionFavorite);
-			}
+				$(element).wrap( "<span></span>" );
+				element.style.outline = '2.5px solid blue';
+				element.style.backgroundColor = 'lightBlue';
+			    element.addEventListener("mouseover", mouseOver);
+				element.addEventListener("mouseout", mouseOut);
+				if (i == 0)
+				{
+				    element.addEventListener("click", actionRetweet);
+				}
+				if (i == 1)
+				{
+					element.addEventListener("click", actionFavorite);
+				}
+		    }
 		}
 	}
 
     // Followers and Following from newsfeed page
-	var lis = document.getElementsByClassName("ProfileCardStats-statList Arrange Arrange--bottom Arrange--equal")[0];  
+	var lis = document.getElementsByClassName("ProfileCardStats-statList Arrange Arrange--bottom Arrange--equal")[0];
 	if (lis != null)
 	{
 		lis = lis.getElementsByTagName("li");
 		for (var j = 1; j < lis.length; j++) {
-			lis[j].style.outline = '1.5px solid blue';
-			lis[j].style.backgroundColor = 'lightBlue';
-		    lis[j].addEventListener("mouseover", mouseOver);
-			lis[j].addEventListener("mouseout", mouseOut);
-			if (j == 1)
+			if (lis[j].style.outline == '')
 			{
-				lis[j].addEventListener("click", actionFollowing);
-			}
-			if (j == 2)
-			{
-				lis[j].addEventListener("click", actionFollower);
+				lis[j].style.outline = '1.5px solid blue';
+				lis[j].style.backgroundColor = 'lightBlue';
+			    lis[j].addEventListener("mouseover", mouseOver);
+				lis[j].addEventListener("mouseout", mouseOut);
+				if (j == 1)
+				{
+					lis[j].addEventListener("click", actionFollowing);
+				}
+				if (j == 2)
+				{
+					lis[j].addEventListener("click", actionFollower);
+				}
 			}
 		}
 	}
@@ -105,20 +183,25 @@ function highlight() {
     if (lis2 != null)
 	{
 		lis2 = lis2.getElementsByTagName("li");
-	    for (var k = 1; k < 3; k++)
+	    for (var k = 0; k < lis2.length; k++)
 		{
-			lis2[k].style.outline = '1.5px solid blue';
-			lis2[k].style.backgroundColor = 'lightBlue';
-		    lis2[k].addEventListener("mouseover", mouseOver);
-			lis2[k].addEventListener("mouseout", mouseOut);
-			if (k == 1)
+			if (((lis2[k].getAttribute('class') == 'ProfileNav-item ProfileNav-item--following') || (lis2[k].getAttribute('class') == 'ProfileNav-item ProfileNav-item--followers') ||
+				(lis2[k].getAttribute('class') == 'ProfileNav-item ProfileNav-item--followers is-active') ||
+				(lis2[k].getAttribute('class') == 'ProfileNav-item ProfileNav-item--following is-active')) && (lis2[k].style.outline == ''))
 			{
-				lis2[k].addEventListener("click", actionFollowing);
-			}
-			if (k == 2)
-			{
-				lis2[k].addEventListener("click", actionFollower);
-			}
+				lis2[k].style.outline = '1.5px solid blue';
+				lis2[k].style.backgroundColor = 'lightBlue';
+			    lis2[k].addEventListener("mouseover", mouseOver);
+				lis2[k].addEventListener("mouseout", mouseOut);
+				if ((lis2[k].getAttribute('class') == 'ProfileNav-item ProfileNav-item--following') || (lis2[k].getAttribute('class') == 'ProfileNav-item ProfileNav-item--following is-active'))
+				{
+					lis2[k].addEventListener("click", actionFollowing);
+				}
+				if ((lis2[k].getAttribute('class') == 'ProfileNav-item ProfileNav-item--followers') || (lis2[k].getAttribute('class') == 'ProfileNav-item ProfileNav-item--followers is-active'))
+				{
+					lis2[k].addEventListener("click", actionFollower);
+				}
+		    }
 		}
 	}
 }
